a8f020c12d863ef54c2ba1ad0de36ae19c661fa4
lostyue <lyon3008@163.com>
2015-12-07 10:00:15 +0800

    稳定版本

--- ltcms/src/main/java/com/lostyue/ltcms/admin/controller/MenuConfigController.java
+++ ltcms/src/main/java/com/lostyue/ltcms/admin/controller/MenuConfigController.java
@@ -3,19 +3,12 @@
  */
 package com.lostyue.ltcms.admin.controller;
 
-import java.util.Calendar;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.lostyue.ltcms.admin.dao.MenuDao;
-import com.lostyue.ltcms.admin.model.Menu;
 import com.lostyue.ltcms.admin.service.MenuService;
 
 /**
@@ -36,11 +29,7 @@ public class MenuConfigController {
 	 */
 	@RequestMapping("/list")
 	public String showMenuList(Model model){
-		Menu m = new Menu();
-		m.setCreate_date(Calendar.getInstance().getTime());
-		m.setLt_menu_code("menu code");
-		m.setLt_menu_name("menu name");
-		menuService.saveMenu(m);
+		menuService.saveMenu();
 		
 		return "admin/menu/menuList";
 	}
--- ltcms/src/main/java/com/lostyue/ltcms/admin/service/MenuService.java
+++ ltcms/src/main/java/com/lostyue/ltcms/admin/service/MenuService.java
@@ -1,33 +1,19 @@
 package com.lostyue.ltcms.admin.service;
 
-import java.util.UUID;
-
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.lostyue.ltcms.admin.dao.MenuDao;
-import com.lostyue.ltcms.admin.dao.NewMenuDao;
-import com.lostyue.ltcms.admin.model.Menu;
 
 @Component("menuService")
 public class MenuService {
 	
 	@Autowired
-	@Qualifier("newMenuDao")
-	private NewMenuDao newMenuDao;
-	
-	@Autowired
 	@Qualifier("menuDao")
 	private MenuDao menuDao;
 	
-	public void saveMenu(Menu m){
-		if(StringUtils.isBlank(m.getLt_menu_id())){
-			m.setLt_menu_id(UUID.randomUUID().toString());
-		}
-		newMenuDao.save(m);
-		
+	public void saveMenu(){
 		menuDao.newMenu();
 	}
 }
